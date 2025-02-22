6fc6887ae5a940b6252df6bff295eb10cadc42f1
Abednego Tenge <abednegotenge180@gmail.com>
2025-02-22 19:51:46 +0000

    another commit

--- chatbot/bot.py
+++ chatbot/bot.py
@@ -1,17 +1,20 @@
 import random
 import json
 from chatbot.nlp_utils import preprocess_text
+from difflib import get_close_matches
 
 # Load intents from the JSON file
 with open('chatbot/intents.json') as file:
     intents = json.load(file)
 
 def get_response(user_input):
-    user_token = set(preprocess_text(user_input).split())
+    user_input = preprocess_text(user_input)
 
-    for intent in intents['intents']:
-        for pattern in intent['patterns']:
-            pattern_token = set(pattern.lower().split())
-            if user_token.intersection(pattern_token):
+    all_patterns = [pattern.lower() for intent in intents['intents'] for pattern in intent['patterns']]
+    closest_match = get_close_matches(user_input.lower(), all_patterns, n=1, cutoff=0.7)
+
+    if closest_match:
+        for intent in intents['intents']:
+            if closest_match[0] in map(str.lower, intent['patterns']):    
                 return random.choice(intent['responses'])
     return "I'm not sure how to respond to that. Can you rephrase?"
